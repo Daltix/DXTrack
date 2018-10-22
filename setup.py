89a65368c7c59a9c4bc3c73197c054ce2272ed8e
Sam Hopkins <hopkins.samuel@gmail.com>
2018-10-22 15:00:16 +0100

    no library code changes, just infrastructure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dxtrack',
-    version='0.4',
+    version='0.4.1',
     description='Python utilities and aws service for Daltix metric and error '
                 'tracking',
     author='Sam Hopkins',
