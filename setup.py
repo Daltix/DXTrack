16ec35030d4f4514ea24d55dc9d74f337ccc3647
Sam Hopkins <hopkins.samuel@gmail.com>
2018-10-31 10:40:52 +0000

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dxtrack',
-    version='0.5.0',
+    version='0.6.1',
     description='Python utilities and aws service for metric and error '
                 'tracking',
     author='Sam Hopkins',
