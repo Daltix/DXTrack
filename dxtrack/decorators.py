00fdfa252f499658ffbbdcada3ac96bfdf47f1e5
Louis Cuthbert <louis@daltix.com>
2019-07-04 16:24:19 +0100

    :recycle: Moving to 80 character limit

--- dxtrack/decorators.py
+++ dxtrack/decorators.py
@@ -4,8 +4,10 @@ import asyncio
 def fire_and_forget(f):
     """
 
-    A function that can be used as a decorator to execute the function asynchronously.
-    https://stackoverflow.com/questions/37278647/fire-and-forget-python-async-await/37345564
+    A function that can be used as a decorator to execute the function
+    asynchronously.
+    https://stackoverflow.com/questions/37278647/fire-and-forget-
+    python-async-await/37345564
 
     :param f: The function f run as async.
 
@@ -13,6 +15,8 @@ def fire_and_forget(f):
 
     """
     def wrapped(*args, **kwargs):
-        return asyncio.get_event_loop().run_in_executor(None, f, *args, *kwargs)
+        return asyncio.get_event_loop().run_in_executor(
+            None, f, *args, *kwargs
+        )
 
     return wrapped
--- tests/test_async.py
+++ tests/test_async.py
@@ -13,13 +13,15 @@ MAXIMUM_ASYNC_EXECUTION_TIME = 0.05
 class TestAsyncBehaviour(unittest.TestCase):
     """
 
-    A test class that runs DXTrack in both synchronous and asynchronous mode. We override the function that would be
-    used to send metrics with a function that takes 1 second to execute. (Although this should probably be a mocked
-    version of the object).
+    A test class that runs DXTrack in both synchronous and asynchronous mode.
+    We override the function that would be used to send metrics with a
+    function that takes 1 second to execute. (Although this should probably
+    be a mocked version of the object).
 
-    To verify that the functionality is working as expected we send a metric via DXTrack that executes our sleep
-    function. If everything is as expected, when running in async mode there should be almost no delay to get to the
-    next line in the text.
+    To verify that the functionality is working as expected we send a metric
+    via DXTrack that executes our sleep function. If everything is as
+    expected, when running in async mode there should be almost no delay to
+    get to the next line in the text.
 
     """
     def test_async_is_non_blocking(self):
@@ -32,15 +34,18 @@ class TestAsyncBehaviour(unittest.TestCase):
             use_async_requests=True
         )
         #
-        #   Override our default write out function with our one that sleeps synchronously.
+        #   Override our default write out function with our one that sleeps
+        #   synchronously.
         dx._write_out = one_second_function
         #
-        #   Take a look at the time now, run our function, and check how long it took.
+        #   Take a look at the time now, run our function, and check how
+        #   long it took.
         then = time.time()
         dx.metric("a", 0)
         now = time.time()
         #
-        #   Check that the difference in time is no longer than our allotted maximum execution time for async functions.
+        #   Check that the difference in time is no longer than our allotted
+        #   maximum execution time for async functions.
         assert now - then < MAXIMUM_ASYNC_EXECUTION_TIME
 
     def test_sync_is_blocking(self):
@@ -53,13 +58,16 @@ class TestAsyncBehaviour(unittest.TestCase):
             use_async_requests=False
         )
         #
-        #   Override our default write out function with our one that sleeps synchronously.
+        #   Override our default write out function with our one that sleeps
+        #   synchronously.
         dx._write_out = one_second_function
         #
-        #   Take a look at the time now, run our function, and check how long it took.
+        #   Take a look at the time now, run our function, and check how long
+        #   it took.
         then = time.time()
         dx.metric("a", 0)
         now = time.time()
         #
-        #   Check that the difference in time is longer than our allotted maximum execution time for async functions.
+        #   Check that the difference in time is longer than our allotted
+        #   maximum execution time for async functions.
         assert now - then > MAXIMUM_ASYNC_EXECUTION_TIME
